e0c36d9bd27778ce9602ebef97908049b1ed62de
action0709 <action0709@mail.ru>
2023-07-25 20:25:17 +0400

    1

---
+++ src/main/java/com/SkyPro/kursovik_2kurs/Kursovik2kursApplication.java
@@ -0,0 +1,13 @@
+package com.SkyPro.kursovik_2kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovik2kursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursovik2kursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/SkyPro/kursovik_2kurs/Kursovik2kursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SkyPro.kursovik_2kurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursovik2kursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
